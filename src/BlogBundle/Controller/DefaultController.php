0a716c4620586c9a06eb937fa7770126a05a0ffe
Diego Abanto <diego.abanto@ec.pe>
2017-03-27 10:05:15 -0500

    many to many

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    // RELACIONES MUCHOS A MUCHOS
     public function indexAction()
     {
         //Ejemplo: Listar todas las entradas
-        $em = $this->getDoctrine()->getManager();
-        $entry_repo = $em->getRepository("BlogBundle:Category");
+        //$em = $this->getDoctrine()->getManager();
+        $em =  $this->getDoctrine()->getEntityManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
         
+        foreach($entries as $entry){
+            echo $entry->getTitle() . "<br/>";
+            echo $entry->getCategory()->getName() . "<br/>";
+            echo $entry->getUser()->getName() . "<br/><hr>";
+        }
+        
+        die();
         
         return $this->render('BlogBundle:Default:index.html.twig');
     }
